10d97797b317e305e5102297fc87b1ec9ae86a6c
tumpa <tumpa1634@gmail.com>
2024-04-22 20:07:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("chai aur code");
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+      res.send('ishachy!')
+    })
+
+    app.get('/facebook', (req, res) => {
+      res.send('<h1>ishaishaisha</h1>')
+    })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
